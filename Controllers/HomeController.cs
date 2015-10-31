46f6acf3aa53942b27bc27f356315c112b17d26a
Armen Shimoon <armen.shimoon@gmail.com>
2015-10-31 12:46:15 -0700

    Initial checkin for demo.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Configuration;
+
+namespace DockerEnvDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = _config.GetSection("MySetting").Value;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
